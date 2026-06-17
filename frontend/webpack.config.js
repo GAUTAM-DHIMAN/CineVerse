3beaf50cc3af81545f98a3323c85c8c06753bbef
Gautam Dhiman <gautamdhimanamb@gmail.com>
2026-06-18 00:15:28 +0530

    frontend: define process.env.REACT_APP_API_URL in Webpack configuration to prevent ReferenceError

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -18,6 +19,11 @@ module.exports = {
     historyApiFallback: true,
     port: 3000
   },
-  plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
+  plugins: [
+    new HtmlWebpackPlugin({ template: './public/index.html' }),
+    new webpack.DefinePlugin({
+      'process.env.REACT_APP_API_URL': JSON.stringify(process.env.REACT_APP_API_URL || 'http://localhost:8080/api')
+    })
+  ],
   resolve: { extensions: ['.js', '.jsx'] }
 };
